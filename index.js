75a130fef4604025c851a809d5b1621062eafdb1
dyl-armstead <67605274+dyl-armstead@users.noreply.github.com>
2020-07-26 11:56:22 -0700

    initial upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$(function() {
+    $("#js-shopping-list-form").find('button').click(event => {
+      event.preventDefault();
+      let newItem = $('form').find(
+      'input[name="shopping-list-entry"]').val();
+      $("ul").append(
+        `<li><span class="shopping-item">${newItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button>    <button class="shopping-item-delete"><span class="button-label">delete</span></button></div>
+        </li>`);
+        });
+  
+    $('ul').on('click','button.shopping-item-toggle',function(event) {
+      event.preventDefault();
+      $(event.currentTarget).closest('li').find('span').toggleClass('shopping-item__checked');
+    });
+  
+    $('ul').on('click','button.shopping-item-delete',function(event) {
+      event.preventDefault();
+      $(event.currentTarget).closest('li').remove('li')
+    });
+  });
